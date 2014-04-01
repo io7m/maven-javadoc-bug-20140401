14b6495c718373bcecac36f55d2eef26b2167a08
Mark R <code@io7m.com>
2014-04-01 19:46:21 +0000

    Initial files

---
+++ lib1/src/main/java/com/io7m/example/lib1/Lib1Example.java
@@ -0,0 +1,10 @@
+package com.io7m.example.lib1;
+
+/**
+ * An example.
+ */
+
+public final class Lib1Example
+{
+
+}
---
+++ lib2/src/main/java/com/io7m/example/lib2/Lib2Example.java
@@ -0,0 +1,10 @@
+package com.io7m.example.lib2;
+
+/**
+ * An example.
+ */
+
+public class Lib2Example
+{
+
+}
